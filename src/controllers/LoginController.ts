1200ee819f9e04a201fb578523d9d163b9f8aa96
Marcelo Augusto <maraceloaugust@gmail.com>
2023-04-26 11:28:49 -0300

    HTML de login e cadastro adicionados

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express";
 
 export function login(req: Request, res: Response){
-    res.render('login/login');
+    res.render('login/login', {
+        title: 'Login',
+        pagecss: 'login.css',
+    });
 }
 
 export function register(req: Request, res: Response){
-    res.render('login/register');
+    res.render('login/register', {
+        title: 'Cadastro',
+        pagecss: 'login.css',
+    });
 };
 
 export function logout(req: Request, res: Response){
