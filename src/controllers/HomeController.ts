d98ceecb8ef391da038cf2240d4b0f20d6d7b025
Marcelo Augusto <maraceloaugust@gmail.com>
2023-09-02 23:27:21 -0300

    Home informando o usuário lougado

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -4,5 +4,5 @@ import { UserInstance } from "../models/User";
 export async function home(req: Request, res: Response){
     const user = res.locals.user as UserInstance;
 
-    res.json({ success: `${user.name}(${user.email}) está autorizado a acessar a Home` });
+    res.json({ success: `${user.name}(${user.email}) está autorizado a acessar à página Home` });
 }
