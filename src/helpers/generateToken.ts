d2d88c961e8a0e5e9580799a0302c5111203e83b
Marcelo Augusto <maraceloaugust@gmail.com>
2023-10-20 19:14:52 -0300

    Lança Erro se data for vazio

--- src/helpers/generateToken.ts
+++ src/helpers/generateToken.ts
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 
 async function generateToken(data: any, exp: number): Promise<string>{
     try{
+        if(data === '') throw Error('Data is Empty');
         return jwt.sign(data, process.env.JWT_SECRET_KEY as string, {expiresIn: exp});
     }catch(err){
         console.log(err);
